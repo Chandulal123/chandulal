591f667cc0b56e2f8da1d38e9ecb6d9264ada459
Chandulal123 <cc.5759001@gmail.com>
2025-07-02 02:08:28 +0530

    Add files via upload

---
+++ python learning project.py
@@ -0,0 +1,25 @@
+#task 1 : basic python mathematical operation
+print('assignment1 task 1')
+
+
+number1=input('enter the first number :' )
+number2=input('enter the second number :')
+
+a=int(number1)
+b=int(number2)
+
+
+print('addition ;' ,a+b)
+print('subtraction :' ,a-b)
+print('multification ;' ,a*b)
+print('division ;' ,a/b)
+
+# personalized greeting : task 2
+print('assignment1 task 2')
+
+user_namef=input('users first name :')
+user_namel=input('users last name :')
+user_name=(user_namef + user_namel)
+
+
+print("hello,"+user_name+"!" " wellcome to the python program")
